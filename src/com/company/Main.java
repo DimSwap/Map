6e8aa6fc565c4d16bf1b9ac2c618eeaffb891f34
DimSwap <logu123dima@gmail.com>
2021-11-03 00:00:54 +0200

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,20 @@
+package com.company;
+public class Main {
+
+    public static void main(String[] args) {
+        MyMap myMap = new MyHashMap();
+        myMap.put("S", "35");
+        myMap.put("M", "38");
+        myMap.put("L", "40");
+        myMap.put("XL", "42");
+        System.out.println(myMap.containsKey("M"));
+
+        MySet set = new MyHashSet();
+        set.add("Dima");
+        set.add("Damir");
+        set.add("Vlad");
+        set.add("BMW");
+        System.out.println(set.size());
+    }
+
+}
---
+++ src/com/company/MySet.java
@@ -0,0 +1,17 @@
+package com.company;
+
+public interface MySet {
+    boolean add(String e);
+
+    void clear();
+
+    boolean contains(String o);
+
+    boolean isEmpty();
+
+    boolean remove(String o);
+
+    Object size();
+
+    String[] toArray();
+}
